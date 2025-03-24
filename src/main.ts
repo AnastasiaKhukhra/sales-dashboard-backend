47a78dcbcb2b367303e2b7f6cacfca351ec585a7
Anastasiia Khukhra <anastasiiakhukhra@MacBook-Pro-Anastasiia.local>
2025-03-24 18:31:04 +0200

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Configure CORS
   app.enableCors({
     origin: [
       'http://localhost:5173',
-      'https://sales-dashboard-frontend.onrender.com'
+      'https://sales-dashboard-frontend-its6.onrender.com'
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
